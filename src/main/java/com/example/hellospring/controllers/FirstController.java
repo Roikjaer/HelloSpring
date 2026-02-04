d692fc3d803fd6c4c4c899a07ba4d5dbb2958a8a
Maltheroi <maro0013@stud.ek.dk>
2026-02-04 18:19:15 +0100

    init commit

--- src/main/java/com/example/hellospring/controllers/FirstController.java
+++ src/main/java/com/example/hellospring/controllers/FirstController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
 @RestController
 public class FirstController {
 
@@ -19,11 +22,56 @@ public class FirstController {
         return message;
     }
 
-    @GetMapping("/example")
-    public String params(@RequestParam String p1){
-        if(p1.equals("nicklas")){
-            return "Goated softwareudvikler";
+
+    @GetMapping("/echo")
+    public String params(@RequestParam String input){
+        if(input.equalsIgnoreCase("HelloData-GBG-E25D")){
+            return "HelloDATA-GBG-E25D";
         }
-        return "Ingen";
+        else{
+            return "Hov nej tak";
+        }
     }
+
+   @GetMapping("/length")
+   public String params1(@RequestParam String text){
+        return String.valueOf(text.length());
+
+
+   }
+
+    @GetMapping("/hello")
+    public String secondEndPoint(){
+        String sødBesked = "Hej hav en mega fed dag";
+
+        return sødBesked;
+    }
+
+   @GetMapping("/word-count")
+    public String params2(@RequestParam String text){
+        int countWords = text.split("\\s").length;
+        return String.valueOf(countWords);
+
+
+
+    }
+
+    @GetMapping("/until-friday")
+    public String erDetFredag(){
+        int dayNumber = LocalDate.now().getDayOfWeek().getValue();
+        boolean fredag;
+        if(dayNumber == 5){
+            fredag = true;
+        } else {
+            fredag = false;
+        }
+
+        if(fredag){
+            return "style=\"color:MediumSeaGreen;\" > YES<b>";
+        }
+        else return "style=\"color:Tomato;\"NO - Days till friday: " + (5 - dayNumber + 7) % 7;
+
+    }
+
+
 }
